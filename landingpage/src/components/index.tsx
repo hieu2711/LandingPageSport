635625a77003b10cf3a874fdae368619a912d2c0
Hieu <2051052042hieu@ou.edu.vn>
2024-07-23 00:16:50 +0700

    uploads

---
+++ landingpage/src/components/index.tsx
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar/Navbar"
+import Home from "./Home/Home"
+import Benefits from "./Benefits/index"
+import OurClasses from "./OurClasses/OurClasses"
+import ContactUs from "./ContactUs/ContactUs"
+export  { Navbar, Home, Benefits , OurClasses, ContactUs};
